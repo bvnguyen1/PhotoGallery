5764e9a80049fbcd9d4967098471265b2ef272b2
CaptainBamboo <48262597+captainbamboo@users.noreply.github.com>
2021-03-25 20:10:25 -0700

    Photo Gallery

--- app/src/main/java/com/bignerdranch/android/photogallery2/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery2/PhotoGalleryFragment.kt
@@ -6,10 +6,9 @@ import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
+import androidx.appcompat.widget.SearchView
 import android.widget.TextView
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
@@ -20,115 +19,6 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.photogallery2.ThumbnailDownloader
 
-
-
-//private const val TAG = "PhotoGalleryFragment"
-//
-//class PhotoGalleryFragment : Fragment() {
-//
-//    private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
-//    private lateinit var photoRecyclerView: RecyclerView
-//    private lateinit var thumbnailDownloader: ThumbnailDownloader<PhotoHolder>
-//
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        retainInstance = true
-//
-//        photoGalleryViewModel =
-//                ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
-//
-//        val responseHandler = Handler()
-//        thumbnailDownloader =
-//            ThumbnailDownloader(responseHandler) { photoHolder, bitmap ->
-//                val drawable = BitmapDrawable(resources, bitmap)
-//                photoHolder.bindDrawable(drawable)
-//            }
-//        lifecycle.addObserver(thumbnailDownloader.fragmentLifecycleObserver)
-//
-//    }
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        viewLifecycleOwner.lifecycle.addObserver(
-//            thumbnailDownloader.viewLifecycleObserver
-//        )
-//
-//        val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
-//
-//        photoRecyclerView = view.findViewById(R.id.photo_recycler_view)
-//        photoRecyclerView.layoutManager = GridLayoutManager(context, 3)
-//
-//        return view
-//    }
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        photoGalleryViewModel.galleryItemLiveData.observe(
-//            viewLifecycleOwner,
-//            Observer { galleryItems ->
-//                Log.d(TAG, "Have gallery items from view model $galleryItems")
-//                photoRecyclerView.adapter = PhotoAdapter(galleryItems)
-//            })
-//    }
-//
-//    override fun onDestroyView() {
-//        super.onDestroyView()
-//        viewLifecycleOwner.lifecycle.removeObserver(
-//            thumbnailDownloader.viewLifecycleObserver
-//        )
-//    }
-//
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        lifecycle.removeObserver(
-//            thumbnailDownloader.fragmentLifecycleObserver
-//
-//        )
-//    }
-//
-//    private class PhotoHolder(private val itemImageView: ImageView)
-//        : RecyclerView.ViewHolder(itemImageView) {
-//        val bindDrawable: (Drawable) -> Unit = itemImageView::setImageDrawable
-//    }
-//
-//    private inner class PhotoAdapter(private val galleryItems: List<GalleryItem>)
-//        : RecyclerView.Adapter<PhotoHolder>() {
-//
-//        override fun onCreateViewHolder(
-//            parent: ViewGroup,
-//            viewType: Int
-//        ): PhotoHolder {
-//            val view = layoutInflater.inflate(
-//                R.layout.list_item_gallery,
-//                parent,
-//                false
-//            ) as ImageView
-//            return PhotoHolder(view)
-//
-//        }
-//
-//        override fun getItemCount(): Int = galleryItems.size
-//
-//        override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
-//            val galleryItem = galleryItems[position]
-//            val placeholder: Drawable = ContextCompat.getDrawable(
-//                requireContext(),
-//                R.drawable.bill_up_close
-//            ) ?: ColorDrawable()
-//            holder.bindDrawable(placeholder)
-//            thumbnailDownloader.queueThumbnail(holder, galleryItem.url)
-//
-//        }
-//    }
-//
-//    companion object {
-//        fun newInstance() = PhotoGalleryFragment()
-//    }
-//}
 
 private const val TAG = "PhotoGalleryFragment"
 
@@ -142,6 +32,8 @@ class PhotoGalleryFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
         retainInstance = true
+        setHasOptionsMenu(true)
+
 
         photoGalleryViewModel =
             ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
@@ -194,6 +86,45 @@ class PhotoGalleryFragment : Fragment() {
             thumbnailDownloader.fragmentLifecycleObserver
         )
     }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_photo_gallery, menu)
+
+        val searchItem: MenuItem = menu.findItem(R.id.menu_item_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.apply {
+
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(queryText: String): Boolean {
+                    Log.d(TAG, "QueryTextSubmit: $queryText")
+                    photoGalleryViewModel.fetchPhotos(queryText)
+                    return true
+                }
+                override fun onQueryTextChange(queryText: String): Boolean {
+                    Log.d(TAG, "QueryTextChange: $queryText")
+                    return false
+                }
+            })
+            setOnSearchClickListener {
+                searchView.setQuery(photoGalleryViewModel.searchTerm, false)
+            }
+
+
+
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_item_clear -> {
+                photoGalleryViewModel.fetchPhotos("")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 
     private class PhotoHolder(private val itemImageView: ImageView) : RecyclerView.ViewHolder(itemImageView) {
 
